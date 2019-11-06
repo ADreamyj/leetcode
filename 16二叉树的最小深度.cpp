4d8be8f8f83dfd52447ff0d0448e4cdda889e03a
ADreamyj <861396671@qq.com>
2019-11-06 18:16:14 +0800

    16二叉树的最小深度.cpp

---
+++ 16二叉树的最小深度.cpp
@@ -0,0 +1,61 @@
+//#define _CRT_SECURE_NO_WARNINGS  1
+//
+//#include<iostream>
+//#include<vector>
+//#include<queue>
+//using namespace std;
+//struct TreeNode
+//{
+//	TreeNode* _left;
+//	TreeNode* _right;
+//	int _val;
+//	TreeNode(int val)
+//		:_left(nullptr)
+//		, _right(nullptr)
+//		, _val(val)
+//	{}
+//};
+//int maxdeep(TreeNode* head)
+//{
+//	/*f (head == nullptr)
+//	return 0;
+//	int left = maxdeep(head->_left);
+//	int right = maxdeep(head->_right);
+//	return left > right ? right + 1 : left + 1;*/
+//	if (head == nullptr)
+//		return 0;
+//
+//	queue<TreeNode*> q;
+//	q.push(head);
+//	int deep = 0;
+//	while (!q.empty())
+//	{
+//		deep++;
+//		int n = q.size();
+//		while (n--)
+//		{
+//			TreeNode* node = q.front();
+//			q.pop();
+//			if (node->_left == nullptr && node->_right == nullptr)
+//				return deep;
+//			if (node->_left)
+//				q.push(node->_left);
+//			if (node->_right)
+//				q.push(node->_right);
+//		}
+//	}
+//	return deep;
+//}
+//int main()
+//{
+//	TreeNode* a1 = new TreeNode(1);
+//	TreeNode* a2 = new TreeNode(2);
+//	TreeNode* a3 = new TreeNode(3);
+//	TreeNode* a4 = new TreeNode(4);
+//	a1->_left = a2;
+//	a1->_right = a3;
+//	a2->_left = a4;
+//	cout << maxdeep(a1) << endl;;
+//	system("pause");
+//	return 0;
+//}
