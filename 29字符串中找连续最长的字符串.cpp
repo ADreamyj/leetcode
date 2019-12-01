f49712872f3198208452d9f32225dfa061b958ff
ADreamyj <861396671@qq.com>
2019-12-01 10:11:30 +0800

    29字符串中找连续最长的字符串.cpp

---
+++ 29字符串中找连续最长的字符串.cpp
@@ -0,0 +1,31 @@
+//#define _CRT_SECURE_NO_WARNINGS  1
+//
+////字符串中找连续最长的字符串
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string s;
+//	getline(cin, s);
+//
+//	string ret;
+//	string temp;
+//	for (int i = 0; i <= s.size(); i++)//
+//	{
+//		if (s[i] <= '9' && s[i] >= '0')
+//		{
+//			temp += s[i];
+//		}
+//		else
+//		{
+//			if (temp.size() > ret.size())
+//				ret = temp;
+//			else
+//				temp.clear();
+//		}
+//	}
+//	cout << ret << endl;
+//	system("pause");
+//	return 0;
+//}
