f98972352bda0872635893f12750a9ec617a2b9a
ADreamyj <861396671@qq.com>
2019-12-08 21:51:22 +0800

    31二维数组中的查找.cpp

---
+++ 31二维数组中的查找.cpp
@@ -0,0 +1,37 @@
+//#define _CRT_SECURE_NO_WARNINGS  1
+//
+#include<iostream>
+#include<vector>
+using namespace std;
+//bool pathfind(vector<vector<int>> v, int n)
+//{
+//	int rows = v.size();
+//	int cols = v[0].size();
+//	int i, j;
+//	for (i = rows - 1, j = 0; i >= 0 && j < cols;)
+//	{
+//		if (v[i][j] == n)
+//			return true;
+//		else if (v[i][j] > n)
+//		{
+//			i--;
+//		}
+//		else
+//		{
+//			j++;
+//		}
+//	}
+//	return false;
+//}
+//int main()
+//{
+//	vector<vector<int>> v = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+//	cout << pathfind(v, 18);
+//	system("pause");
+//	return 0;
+//}
+
+
+
+
+
