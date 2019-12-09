4b9109b1b8878aced9fd3be43eaa5769eeaf4592
ADreamyj <861396671@qq.com>
2019-12-09 18:53:33 +0800

    32c++实现字符分割.cpp

---
+++ 32c++实现字符分割.cpp
@@ -0,0 +1,46 @@
+//#define _CRT_SECURE_NO_WARNINGS  1
+//
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//using namespace std;
+//
+//void plish(char * s, char demo)
+//{
+//	vector<string> str;
+//	char*  head = s, *tail = s;
+//	while (*head != '\0')
+//	{
+//		while (*head != '\0'&& *head == demo)
+//		{
+//			head++;
+//		}
+//		tail = head;
+//		while (tail != '\0' && *tail != demo)
+//		{
+//			tail++;
+//		}
+//		if (head != tail)
+//		{
+//			str.push_back(string(head, tail));
+//			head = tail;
+//		}
+//		else
+//		{
+//			break;
+//		}
+//	}
+//	for (int i = 0; i < str.size(); i++)
+//	{
+//		cout << str[i] << endl;
+//	}
+//}
+//int main()
+//{
+//	//char* s = "i am a student";
+//	//plish(s,' ');
+//	//cout << n << endl;
+//	system("pause");
+//	return 0;
+//}
+
